5d33f1e6ab71195851e95b7372a367035f8ded5d
Uttam Sutariya <pateluttam171210@gmail.com>
2023-02-26 17:03:12 +0530

    mongodb service communicate to node server

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+const port = process.env.PORT || 6060;
+
+const { MONGO_IP, MONGO_PASSWORD, MONGO_USER, MONGO_PORT } = require("./config");
+
+mongoose.set("strictQuery", true);
+mongoose
+	// here @Mongo:27017 is service name defined in docker-compose, we don't need to specify ip address of that mongodb running container
+	.connect(`mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`)
+	.then(() => {
+		console.log("db connected");
+		app.listen(port, () => console.log(`server listening on port: ${port}`));
+	})
+	.catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
 	res.send("hi node-docker 👋");
 });
-
-const port = process.env.PORT || 6060;
-
-app.listen(port, () => console.log(`server listening on port: ${port}`));
